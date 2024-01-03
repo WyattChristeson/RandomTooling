7d997b76abe54760e49feaf94ff57413305b0ea8
WyattChristeson <133052730+WyattChristeson@users.noreply.github.com>
2024-01-02 18:42:05 -0700

    Create StringRotate.py
    Simple string rotation tool created for a project

---
+++ StringRotate.py
@@ -0,0 +1,14 @@
+def rotate_string(string, direction, n)
+	total = len(string)
+	if n le total
+		total % n
+		if direction = left
+			return string[n]:[total] + string[0]:[n]
+		elif direction == right
+			return string[0]:[-n] + string[-n]:[total]
+		else:
+			raise ErrorStatement
+	else:
+		return ErrorStatement
+
+#Original request: Given a string, a direction, and an integer, rotate the string the given number of times in the given direction
