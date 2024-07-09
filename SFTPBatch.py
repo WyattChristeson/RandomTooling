e0bc823f39361e690e96401530564118461aa27a
WyattChristeson <133052730+WyattChristeson@users.noreply.github.com>
2024-07-09 12:07:40 -0600

    Update SFTPBatch.py

--- SFTPBatch.py
+++ SFTPBatch.py
@@ -32,7 +32,7 @@ def adapt_datetime(dt):
     return dt.isoformat()
 
 def convert_datetime(s):
-    return datetime.datetime.fromisoformat(s)
+    return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
 
 sqlite3.register_adapter(datetime.datetime, adapt_datetime)
 sqlite3.register_converter("timestamp", convert_datetime)
